487ee5db9aa021b311b41ac1a98723e6c62a7627
sesela <sesela@grey>
2015-10-26 01:13:46 +0900

    MyBatis設定まわり修正

--- src/main/java/com/shinya/sample/spring4/web/config/DataConfig.java
+++ src/main/java/com/shinya/sample/spring4/web/config/DataConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.DefaultResourceLoader;
@@ -11,18 +12,21 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternUtils;
 
 @Configuration
-//@MapperScan("com.shinya.sample.spring4.persistence")
+@MapperScan("com.shinya.sample.spring4.web.dao")
 public class DataConfig {
 
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		ResourcePatternResolver resolver = ResourcePatternUtils.getResourcePatternResolver(
-                new DefaultResourceLoader());
 		
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource);
-		sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
 		sessionFactory.setFailFast(true);
-        return sessionFactory.getObject();
+
+		// XMLファイル配置場所が@MapperScanのパスと異なるので別途設定
+		ResourcePatternResolver resolver = ResourcePatternUtils.getResourcePatternResolver(
+                new DefaultResourceLoader());
+		sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
+
+		return sessionFactory.getObject();
 	}
 }
